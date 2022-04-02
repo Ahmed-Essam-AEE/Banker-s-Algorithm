0a42f4b43ff02e35ff031d2819e4a59162e7133b
Ranaihab <rana.ihab7@gmail.com>
2022-04-02 22:33:05 +0200

    safeState function added

--- src/pk/Main.java
+++ src/pk/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 import java.lang.*;
 public class Main {
     static Scanner sc = new Scanner(System.in);
-    public static void fillAvaRes(int m , int [] available)
+    public static void fillInitialRes(int m , int [] available)
     {
         for (int i=0 ; i<m ; i++)
         {
@@ -17,6 +17,13 @@ public class Main {
         {
             System.out.print(available[i] +" ");
         }*/
+    }
+    public static void fillAvaRes(int[]available, int[][]allocated){
+        for(int i =0; i< allocated.length;i++){
+            for(int j =0; j<available.length;j++){
+                available[j]-=allocated[i][j];
+            }
+        }
     }
     public static void fillMax(int n, int m , int [] [] maximum)
     {
@@ -38,13 +45,12 @@ public class Main {
             }
         }
     }
-    public static void fillNeed(int n, int m , int [] [] need)
+    public static void fillNeed(int n, int m , int [][] need, int[][] maxNeed, int[][]allocated)
     {
         for (int i=0 ; i<n ; i++)
         {
             for (int j=0 ; j<m ; j++){
-                System.out.print("Enter remaining needs resources for P"+(i+1)+ " and R" + (j+1) +": ");
-                need[i][j]=sc.nextInt();
+                need[i][j]=maxNeed[i][j]-allocated[i][j];
             }
         }
     }
@@ -67,6 +73,52 @@ public class Main {
 
         }
     }
+    public static boolean safeState(int[][] x, int[][] y, int[] z) {
+        int n = x.length;
+        int m = z.length;
+        int[] available = new int[m];
+        int[][] need = new int[n][m];
+        int[][] allocated = new int[n][m];
+        boolean [] finished = new boolean[n];
+        for(int i =0; i<n;i++){
+            for(int j =0; j<m;j++){
+                available[j]=z[j];
+                need[i][j]=y[i][j];
+                allocated[i][j]=x[i][j];
+            }
+        }
+        for(int i =0; i<n; i++)
+            finished[i]=false;
+        int prevRemaining = n;
+        while(true){
+            int remaining =n;
+            for(int i =0; i<n;i++){
+                int count = 0;
+                if(finished[i]){
+                    remaining--;
+                    continue;
+                }
+                for(int j = 0; j<m;j++){
+                    if(need[i][j]<=available[j])
+                        count++;
+                }
+                if(count==m){
+                    finished[i]=true;
+                    remaining--;
+                    for(int j = 0; j<available.length;j++){
+                        available[j]+=allocated[i][j];
+                    }
+                }
+
+            }
+            if(remaining==prevRemaining)
+                return false;
+            if(remaining==0)
+                break;
+            prevRemaining = remaining;
+        }
+        return true;
+    }
     public static void main(String[] args) {
 
         int n=sc.nextInt() , m=sc.nextInt();                  // n is the number of processes and m is number of resources.
@@ -74,13 +126,13 @@ public class Main {
         int [][] maximum = new int[n][m];                    //the maximum demand of each process.
         int [][] allocation = new int[n][m];                 //the amount currently allocated to each process.
         int [][] need = new int[n][m];                       //the remaining needs of each process.
-        fillAvaRes(m , available);
+        fillInitialRes(m , available);
         fillAlloc(n,m, allocation);
         fillMax(n,m, maximum);
-        fillNeed(n,m,need);
-
-
-
-
+        fillNeed(n,m,need,maximum,allocation);
+        fillAvaRes(available,allocation);
+        System.out.println(safeState(allocation,need,available));
+        for(int i =0;i<available.length;i++)
+            System.out.println(available[i]);
     }
 }
