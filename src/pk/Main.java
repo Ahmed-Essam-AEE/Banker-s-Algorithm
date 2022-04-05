c33afcac40283406a2b0f188a6e75a5753d7d2e2
Ranaihab <rana.ihab7@gmail.com>
2022-04-05 17:41:25 +0200

    recovery, finished

--- src/pk/Main.java
+++ src/pk/Main.java
@@ -1,18 +1,27 @@
 package pk;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
     static Scanner input = new Scanner(System.in);
+    static int[][] allocated;
+    static int[][] requested;
+    static int[][] need;
+    static int[][] maxNeed;
+    static int[] available;
+    static ArrayList<Integer> pendingRequests = new ArrayList<>();
+    static int n, m;
     static int[] deadlockedProcess;
 
-    public static void printInfo(int[][] alloc, int[][] need, int[]available, boolean isBanker){
+    public static void printInfo(int[][] need_, boolean isBanker){
         System.out.println("===Allocated Matrix===");
-        for (int i =0; i< alloc.length;i++){
+        for (int i =0; i< allocated.length;i++){
             System.out.print("P" + (i+1) +" ");
-            for (int j =0; j<alloc[i].length;j++)
-                System.out.print(alloc[i][j]+" ");
+            for (int j =0; j< allocated[i].length;j++)
+                System.out.print(allocated[i][j]+" ");
             System.out.println();
         }
         if(isBanker){
@@ -21,10 +30,10 @@ public class Main {
         }else{
             System.out.println("===Requests Matrix===");
         }
-        for (int i =0; i< need.length;i++){
+        for (int i =0; i< need_.length;i++){
             System.out.print("P" + (i+1) +" ");
-            for (int j =0; j<need[i].length;j++)
-                System.out.print(need[i][j]+" ");
+            for (int j =0; j<need_[i].length;j++)
+                System.out.print(need_[i][j]+" ");
             System.out.println();
         }
         System.out.println("===Available resources===");
@@ -34,19 +43,15 @@ public class Main {
         System.out.println();
     }
 
-    public static void fillInitialRes(int m, int[] available) {
+    public static void fillInitialRes() {
         for (int i = 0; i < m; i++) {
-            System.out.print("Enter available resources for R" + (i + 1) + ": ");
+            System.out.print("Enter initial available resources for R" + (i + 1) + ": ");
             available[i] = sc.nextInt();
 
         }
-        /*for (int i=0 ; i<m ; i++)
-        {
-            System.out.print(available[i] +" ");
-        }*/
     }
 
-    public static void fillAvaRes(int[] available, int[][] allocated) {
+    public static void computeAvaRes() { //Compute remaining available resources
         for (int i = 0; i < allocated.length; i++) {
             for (int j = 0; j < available.length; j++) {
                 available[j] -= allocated[i][j];
@@ -54,28 +59,16 @@ public class Main {
         }
     }
 
-    public static void fillMax(int n, int m, int[][] maximum) {
+    public static void fillMax() {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 System.out.print("Enter maximum demand of P" + (i + 1) + " for R" + (j + 1) + ": ");
-                maximum[i][j] = sc.nextInt();
+                maxNeed[i][j] = sc.nextInt();
             }
         }
     }
 
-    private static void fillNeed(int n, int m, int[][] need, int[][] maximum, int[][] allocation) {
-    }
-
-    public static void fillAlloc(int n, int m, int[][] alloc) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print("Enter currently allocated  resources for P" + (i + 1) + " and R" + (j + 1) + ": ");
-                alloc[i][j] = sc.nextInt();
-            }
-        }
-    }
-
-   public static void computeNeed(int n, int m, int[][] need, int[][] maxNeed, int[][] allocated) {
+    public static void computeNeed() {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 need[i][j] = maxNeed[i][j] - allocated[i][j];
@@ -83,26 +76,25 @@ public class Main {
         }
     }
 
-    public static void request(int p, int m, int[] resources, int[][] need) {
-        System.out.println("Request");
-        for (int i = 0;i<m;i++) System.out.println(resources[i]);
-        for (int i = 0; i < m; i++) {
-            need[p][i] += resources[i];
-            // check for safe state.
 
-        }
-    }
-
-    public static void release(int p, int m, int[] resources, int[][] alloc, int[] available) {
-        System.out.print("Release P"+(p+1));
-        for (int i = 0;i<m;i++) System.out.print(resources[i]);
-        for (int i = 0; i < m; i++) {
-            if (resources[i] <= alloc[p][i]) {
-                available[i] += resources[i];
-                alloc[p][i] -= resources[i];
+    public static void fillAlloc() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("Enter currently allocated  resources for P" + (i + 1) + " and R" + (j + 1) + ": ");
+                allocated[i][j] = sc.nextInt();
             }
         }
     }
+
+    public static void fillReq(){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("Enter currently Requeted  resources for P" + (i + 1) + " and R" + (j + 1) + ": ");
+                requested[i][j] = sc.nextInt();
+            }
+        }
+    }
+
     public static boolean compareTwoArrays(int n , int [] arr1 ,  int [] arr2 ){
         for(int i=0 ; i<n ; i++)
         {
@@ -112,35 +104,25 @@ public class Main {
         }
         return true;
     }
-    public static void banker(int n, int m, int p, int[][] max, int[][] alloc, int[] available , int [][] need , int [] requestedResources){
-        computeNeed(n , m , need , max , alloc);
-        /*
-        Set<Integer> s = null;
-        for (int i = 0; i < n; i++) {
-                if(s.contains(i) &&  compareTwoArrays(m ,need[i] , available))
-                {
-                    release(i ,m , alloc[i] , alloc , available );
-                    s.add(i);
-                    i=0;
-                }
-        }*/
+    public static void banker(int p, int [] requestedResources){
+
         if(compareTwoArrays( m , requestedResources , need[p]))
         {
             if(compareTwoArrays(m , requestedResources , available))
             {
                 for(int j=0; j<m ; j++)
                 {
-                    alloc[p][j]+=requestedResources[j];
+                    allocated[p][j]+=requestedResources[j];
                     available[j]-=requestedResources[j];
                     need[p][j]-=requestedResources[j];
                 }
                 System.out.println("Request is being reviewed(check for safe state).");
-                if(!safeState(alloc , need , available))
+                if(!safeState(need))
                 {
                     System.out.println("Request lead to unsafe state).");
                     for(int j=0; j<m ; j++)
                     {
-                        alloc[p][j]-=requestedResources[j];
+                        allocated[p][j]-=requestedResources[j];
                         available[j]+=requestedResources[j];
                         need[p][j]+=requestedResources[j];
                     }
@@ -156,18 +138,17 @@ public class Main {
 
     }
 
-    public static boolean safeState(int[][] x, int[][] y, int[] z) {
-        int n = x.length;
-        int m = z.length;
-        int[] available = new int[m];
-        int[][] need = new int[n][m];
-        int[][] allocated = new int[n][m];
+    public static boolean safeState(int[][]y) {
+
+        int[] av = new int[m];
+        int[][] need_ = new int[n][m];
+        int[][] alloc = new int[n][m];
         boolean[] finished = new boolean[n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                available[j] = z[j];
-                need[i][j] = y[i][j];
-                allocated[i][j] = x[i][j];
+                av[j] = available[j];
+                need_[i][j] = y[i][j];
+                alloc[i][j] = allocated[i][j];
             }
         }
         for (int i = 0; i < n; i++)
@@ -182,14 +163,14 @@ public class Main {
                     continue;
                 }
                 for (int j = 0; j < m; j++) {
-                    if (need[i][j] <= available[j])
+                    if (need_[i][j] <= av[j])
                         count++;
                 }
                 if (count == m) {
                     finished[i] = true;
                     remaining--;
-                    for (int j = 0; j < available.length; j++) {
-                        available[j] += allocated[i][j];
+                    for (int j = 0; j < av.length; j++) {
+                        av[j] += alloc[i][j];
                     }
                 }
 
@@ -215,19 +196,119 @@ public class Main {
         return true;
     }
 
+    public static void request(int id, int[] resources) {
+        for (int i = 0; i < m; i++) {
+            requested[id][i] += resources[i];
+        }
+        for (int i = 0; i < m; i++) {
+            if (requested[id][i] > available[i]) {
+                System.out.println("There is not enough available resources");
+                pendingRequests.add(id);
+                return;
+            }
+        }
+        System.out.println("Resources allocated successfully");
+        for(int i=0;i<m;i++){
+            available[i]-=resources[i];
+            allocated[id][i]+=resources[i];
+        }
+    }
+
+    public static void release(int id, int[] resources) {
+        boolean flag = true;
+        for (int i = 0; i < m; i++) {
+            if (allocated[id][i] < resources[i]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            for (int i = 0; i < m; i++) {
+                allocated[id][i] -= resources[i];
+                available[i] += resources[i];
+            }
+            System.out.println("Resources released successfully");
+        } else System.out.println("Released resources are greater than allocated ones");
+
+
+        for (int i = 0; i < pendingRequests.size(); i++) {
+            boolean isValid = true;
+            for (int j = 0; j < m; j++) {
+                if (requested[pendingRequests.get(i)][j] > available[i])
+                    isValid = false;
+            }
+            if (isValid){
+                for(int j=0;j<m;j++){
+                    available[j]-=requested[pendingRequests.get(i)][j];
+                    allocated[pendingRequests.get(i)][j]+=requested[pendingRequests.get(i)][j];
+                    requested[pendingRequests.get(i)][j]=0;
+                }
+                System.out.println("Process "+pendingRequests.get(i)+"successfully allocated to resources.");
+                pendingRequests.remove(i);
+            }
+        }
+    }
+    public static void recover(){
+        while (!safeState(requested)){
+            int maxId=maxAllocatedProcess();
+            for(int i=0;i<m;i++){
+                available[i]+=allocated[maxId][i];
+                allocated[maxId][i]=0;
+                requested[maxId][i]=0;
+            }
+            System.out.println("Terminate process " + maxId);
+            pendingRequests.remove(pendingRequests.indexOf(maxId));
+        }
+    }
+    public static int maxAllocatedProcess(){
+        int max=0,sum,id=0;
+        for(int i=0;i<deadlockedProcess.length;i++){
+            sum=0;
+            for(int j=0;j<m;j++){
+                sum+=allocated[deadlockedProcess[i]][j];
+            }
+            if(max<sum){
+                max=sum;
+                id=deadlockedProcess[i];
+            }
+        }
+        return id;
+    }
+
 
     public static void main(String[] args) {
         System.out.println("Enter the number of processes and the number of resources:");
-        int n = sc.nextInt(), m = sc.nextInt();             // n is the number of processes and m is number of resources.
-        int[] available = new int[m];                       //the available amount of each resource.
-        int[][] maximum = new int[n][m];                    //the maximum demand of each process.
-        int[][] allocation = new int[n][m];                 //the amount currently allocated to each process.
-        int[][] need = new int[n][m];                       //the remaining needs of each process.
-        fillInitialRes(m, available);
-        fillAlloc(n, m, allocation);
-        fillMax(n, m, maximum);
-        fillNeed(n, m, need, maximum, allocation);
-        fillAvaRes(available, allocation);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        allocated= new int[n][m];
+        need= new int[n][m];
+        maxNeed= new int[n][m];
+        requested= new int[n][m];
+        available= new int[m];
+        for(int i =0; i<n;i++)
+            for(int j =0; j<m;j++)
+                requested[i][j]=0;
+
+        fillInitialRes();
+        fillAlloc();
+        fillMax();
+        computeNeed();
+        computeAvaRes();
+
+        int []req= new int[m];
+        System.out.println("Test cases for banker");
+        for(int i =0;i<3; i++){
+            System.out.println("Test case "+ (i+1));
+            System.out.print("Process ID: ");
+            int p = sc.nextInt();
+            System.out.print("Requested Resources: ");
+            for (int j =0;j<m; j++){
+                req[i]=sc.nextInt();
+            }
+            banker(p, req);
+            printInfo(need,true);
+        }
+        System.out.println("===========Test Cases Recover===========");
         System.out.println("choose to request, release, recover, or quit:");
         String in="";
         while(!in.equals("Quit")){
@@ -238,7 +319,8 @@ public class Main {
                 String[] splitinput = in.split(" ");
                 int p = Integer.parseInt(splitinput[1]);
                 for(int i = 0 ; i <m;i++) resources[i] = Integer.parseInt(splitinput[i+2]);
-                request(p,m,resources,need);
+                request(p,resources);
+                printInfo(requested,false);
             }
             else if(in.charAt(1)=='L'){
                 //RL <process#> <r1> <r2> <r3>
@@ -246,10 +328,12 @@ public class Main {
                 String[] splitinput = in.split(" ");
                 int p = Integer.parseInt(splitinput[1]);
                 for(int i = 0 ; i <m;i++) resources[i] = Integer.parseInt(splitinput[i+2]);
-                release(p,m,resources,allocation,available);
+                release(p,resources);
+                printInfo(requested,false);
             }
             else{
-                //Recover
+                recover();
+                printInfo(requested,false);
             }
         }
     }
